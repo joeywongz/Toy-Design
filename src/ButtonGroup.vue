2ef9051de10c67e174835e6ffcba766611ad0e77
wangchong <370318843@qq.com>
2020-04-20 00:41:13 +0800

    第一个button不需要margin-left

--- src/ButtonGroup.vue
+++ src/ButtonGroup.vue
@@ -23,19 +23,10 @@
         vertical-align: middle; 
         >.t-button {
             border-radius: 0;
-            margin-left: -1px;
-            &:first-child{
-                border-top-left-radius: var(--border-radius);
-                border-bottom-left-radius: var(--border-radius);
-            }
-            &:last-child{
-                border-top-right-radius: var(--border-radius);
-                border-bottom-right-radius: var(--border-radius);
-            }
-            &:hover{
-                position: relative;
-                z-index: 1;
-            }
+            &:not(:first-child){margin-left: -1px;}
+            &:first-child{border-top-left-radius: var(--border-radius);border-bottom-left-radius: var(--border-radius);}
+            &:last-child{border-top-right-radius: var(--border-radius);border-bottom-right-radius: var(--border-radius);}
+            &:hover{position: relative;z-index: 1;}
         }
     }
 </style>
